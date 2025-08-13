03b34d6a9c2150371b14b202094b0d0eb292c0b7
joyzhang14 <joyzhang14-14@users.noreply.github.com>
2025-08-12 22:11:27 -0400

    Initial commit - Discord Translator Bot for Railway deployment

--- web.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => res.send('Bot is running'));
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Web server listening on ${port}`));
